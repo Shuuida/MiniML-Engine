c76d0f19b9cb1b0a3cecf47fd81e79a1682ca26b
Michego Takoro <154541788+Shuuida@users.noreply.github.com>
2025-12-07 11:37:54 -0400

    Cambios en los paquetes estimators y adapters

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="miniml",
     version="1.0.1",
-    packages=find_packages(),
+    packages=find_packages(include=['miniml*', 'estimators*', 'adapters*']),
     python_requires=">=3.7",
     install_requires=[],
 )
