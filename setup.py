6e508fd092728fa4ca80286795ae76c72b326938
Michego Takoro <154541788+Shuuida@users.noreply.github.com>
2025-12-07 17:30:23 -0400

    Despliegue oficial de la v1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="miniml",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(include=['miniml*', 'estimators*', 'adapters*']),
     python_requires=">=3.7",
     install_requires=[],
